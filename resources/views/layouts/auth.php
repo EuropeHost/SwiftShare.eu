fea16c927fb133fcfb1e4be3800cd674d72e004f
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-06-15 10:59:36 +0200

    Update auth.php

--- resources/views/layouts/auth.php
+++ resources/views/layouts/auth.php
@@ -1,7 +1,7 @@
 @extends('layouts.app') @section('content')
 <div class="flex min-h-screen flex-col items-center justify-center bg-gray-50">
   <div class="mb-6">
-    <a href="/" class="text-3xl font-bold text-gray-900">
+    <a href="{{ route('home') }}" class="text-3xl font-bold text-gray-900">
       {{ config("app.name", "SwiftShare") }}
     </a>
   </div>
