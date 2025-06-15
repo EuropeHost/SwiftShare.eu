197f0b5dfad5bce3ebf04ced6df854805f89ee98
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-06-15 10:59:03 +0200

    Create auth.php

---
+++ resources/views/layouts/auth.php
@@ -0,0 +1,12 @@
+@extends('layouts.app') @section('content')
+<div class="flex min-h-screen flex-col items-center justify-center bg-gray-50">
+  <div class="mb-6">
+    <a href="/" class="text-3xl font-bold text-gray-900">
+      {{ config("app.name", "SwiftShare") }}
+    </a>
+  </div>
+  <div class="w-full max-w-md rounded-lg bg-white p-8 shadow-md">
+    @yield('auth_content')
+  </div>
+</div>
+@endsection
