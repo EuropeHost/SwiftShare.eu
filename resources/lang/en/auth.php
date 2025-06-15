bee4bee09b0be484eb7d4028c2c5f9bab52cf037
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-06-15 11:07:11 +0200

    Create auth.php

---
+++ resources/lang/en/auth.php
@@ -0,0 +1,62 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    "failed" => "These credentials do not match our records.",
+    "password" => "The provided password is incorrect.",
+    "throttle" => "Too many login attempts. Please try again in :seconds seconds.",
+    "discord_failed" => "Could not log in with Discord. Please try again.",
+
+    // Login Page
+    "login_title" => "Sign in to your account",
+    "email" => "Email address",
+    "remember_me" => "Remember me",
+    "forgot_password" => "Forgot your password?",
+    "sign_in" => "Sign In",
+    "or_continue_with" => "Or continue with",
+    "login_with_discord" => "Sign in with Discord",
+    "no_account" => "Don't have an account?",
+    "register_now" => "Register now",
+
+    // Register Page
+    "register_title" => "Create a new account",
+    "confirm_password" => "Confirm Password",
+    "register" => "Register",
+    "already_have_account" => "Already have an account?",
+
+    // Password Reset
+    "forgot_password_text" => "Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.",
+    "send_password_reset_link" => "Email Password Reset Link",
+
+    // Email Verification
+    "verify_email_text" => "Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn't receive the email, we will gladly send you another.",
+    "verify_email_sent" => "A new verification link has been sent to the email address you provided during registration.",
+    "resend_verification_email" => "Resend Verification Email",
+
+    // General
+    "logout" => "Log Out",
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    "passwords" => [
+        "reset" => "Your password has been reset.",
+        "sent" => "We have emailed your password reset link.",
+        "throttled" => "Please wait before retrying.",
+        "token" => "This password reset token is invalid.",
+        "user" => "We can't find a user with that email address.",
+    ],
+];
